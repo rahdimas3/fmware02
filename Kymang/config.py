fa44a45292d272bfe25b7d80d21255e351d95649
Mediafre <118271806+Mediafre@users.noreply.github.com>
2024-05-13 22:36:49 +0700

    Update config.py

--- Kymang/config.py
+++ Kymang/config.py
@@ -7,13 +7,13 @@ from dotenv import load_dotenv
 load_dotenv(".env")
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7190064957:AAFW2GeZqyJ6vSoQAig5m_GtjRdtsdDQrJc")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6938657094:AAFpqnpOrg6Ne90WbhT_-FC8ieLd3UA6IRA")
 API_ID = int(os.environ.get("API_ID", "17131033"))
 API_HASH = os.environ.get("API_HASH", "7768488c115ac09684bb38e608c47997")
-MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://kay123:kay123@cluster0.ivzdirh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-ADMINS = [int(x) for x in (os.environ.get("ADMINS", "1668766845").split())]
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://motof22932:@cluster0.asoxks3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+ADMINS = [int(x) for x in (os.environ.get("ADMINS", "1734774709").split())]
 MEMBER = [int(x) for x in (os.environ.get("MEMBER", "160").split())]
-LOG_GRP = int(os.environ.get("LOG_GRP", "-1002103101233"))
-BOT_ID = int(os.environ.get("BOT_ID", "7190064957"))
+LOG_GRP = int(os.environ.get("LOG_GRP", "-1002061778178"))
+BOT_ID = int(os.environ.get("BOT_ID", "6938657094"))
 
 KITA = [int(x) for x in (os.environ.get("KITA", "1668766845").split())]
