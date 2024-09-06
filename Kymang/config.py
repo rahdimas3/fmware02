cd5cbd8e0f36049194d3c5fbc1354dc27d2d21a3
Mediafre <118271806+Mediafre@users.noreply.github.com>
2024-09-06 13:53:02 +0700

    Update config.py

--- Kymang/config.py
+++ Kymang/config.py
@@ -10,7 +10,7 @@ load_dotenv(".env")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "6938657094:AAFpqnpOrg6Ne90WbhT_-FC8ieLd3UA6IRA")
 API_ID = int(os.environ.get("API_ID", "17131033"))
 API_HASH = os.environ.get("API_HASH", "7768488c115ac09684bb38e608c47997")
-MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://motof22932:@cluster0.asoxks3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://rahdimas2:ZeHzZ3vmz8uD3mit@cluster0.v6muq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 ADMINS = [int(x) for x in (os.environ.get("ADMINS", "1734774709").split())]
 MEMBER = [int(x) for x in (os.environ.get("MEMBER", "160").split())]
 LOG_GRP = int(os.environ.get("LOG_GRP", "-1002061778178"))
